7a3c2bef5f743cbc6c8298257b1427009c863246
Bjfinest98 <opeyemibamigbola54@gmail.com>
2025-06-25 14:19:35 +0100

    commit 1

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -12,9 +12,9 @@ function Navbarcomp() {
         <Navbar.Brand  href="#home" style={{color: 'white'}}><img style={{width:'20%',color:'white',}} src={img1} alt="" /></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav variant="pills" defaultActivekey="/home" className="ms-auto">
+          <Nav variant="pills" defaultActivekey="/" className="ms-auto">
             
-            <Nav.Link href='/home' ><Link to='/'  style={{color: 'white',textDecoration:'none'}}>Home</Link></Nav.Link>
+            <Nav.Link href='/' ><Link to='/' style={{color: 'white',textDecoration:'none'}}>Home</Link></Nav.Link>
             <Nav.Link eventKey="link1"><Link to='/Aboutus' style={{color: 'white',textDecoration:'none'}}>About</Link></Nav.Link>
             <Nav.Link eventKey="link2"><Link to='/Contact us' style={{color: 'white',textDecoration:'none'}}>Contact us</Link></Nav.Link>
             <Nav.Link eventKey="link3"><Link to='Our Service' style={{color: 'white',textDecoration:'none'}}>Explore Now</Link></Nav.Link>
